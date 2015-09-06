ee49ae647385a2a49db074f0256b5b2e039dd2ac
Thijs Kok <mail@thijskok.nl>
2015-09-06 14:18:39 +0200

    Added a short description and an example in the comments

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ var gulp = require('gulp'),
     Elixir = require('laravel-elixir'),
     replace = require('gulp-batch-replace');
 
+/*
+ |----------------------------------------------------------------
+ | Rename Ingredient for Laravel Elixir
+ |----------------------------------------------------------------
+ |
+ | This task will replace occurrences of a specified text search 
+ | pattern into a new text string, just like a "replace all"
+ | function. For example:
+ | 
+ | mix.replace('public/css/app.css', ['images/', '../images/']);
+ |
+ | Effectively, this would prefix every image path with "../".
+ |
+ */
+
 Elixir.extend('replace', function (src, replacements, output) {
     var paths = prepGulpPaths(src, output);
 
